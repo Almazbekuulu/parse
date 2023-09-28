2f21731445daa219a21ddd9a686d6460870325be
Almazbekuulu <almazbekuulutalant20@gmail.com>
2023-09-29 00:33:02 +0600

    new kod

--- scrapy.py
+++ scrapy.py
@@ -1,60 +1,54 @@
+import bs4
 from decouple import config
 import requests
-from bs4 import BeautifulSoup
 import csv
 
-
-
-
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0",
-    "accept": "*/*",
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
+                  'rv:109.0) Gecko/20100101 Firefox/117.0',
+    'accept': '*/*',
+
 }
-
-URL = config("URL")
-CSV_FILE = "softech.csv"
-
-
-def get_response_data(headers, url):
-
-        response = requests.get(url, headers=headers)
-        return response
+URL = config('URL')
+CSV_FILE = 'softech'
 
 
-def content(html_text):
-    soup = BeautifulSoup(html_text, "html.parser")
-
-    all_data = soup.find_all("div", class_="list-item list-label")
-    laptop_content = []
-    for i in all_data:
-        laptop_content.append(
+def content(html_doc):
+    soup = bs4.BeautifulSoup(html_doc, 'lxml')
+    all_info = soup.find_all('div', class_='list-item list-label')
+    content_result_list = []
+    for i in all_info:
+        content_result_list.append(
             {
-                "description": i.find("div", class_="block info-wrapper item-info-wrapper").get_text(),
-                "block price": i.find("div", class_="block price").get_text(),
-                "title": i.find("div", class_="title").get_text().strip(),
-                "image": i.find("img", class_="lazy-image").get("data-src"),
+                'dollars': i.find('div', class_='block price').find('p').find('strong').get_text().strip(),
+                'som': i.find('div', class_='block price').find('p').find('br').next_sibling.get_text().strip(),
+                'name': i.find('h2', class_='name').contents[0].strip(),
+                'photo': i.find('img').get('src'),
+                'info':  ''.join([res for res in i.find('div', class_='block info-wrapper item-info-wrapper').stripped_strings]).replace('.,',''),
+                'views': i.find('span', class_='listing-icons views').get_text().strip(),
 
-                "views": i.find("div",class_="counters").get_text(),
             }
-        )
-    return laptop_content
+         )
+    return content_result_list
 
 
-def save_csv(laptops: list) -> None:
-    with open(CSV_FILE, "w") as f:
-        writer = csv.writer(f, delimiter=",")
-        #writer.writerow(["Название", "Цена", "Картина","Характеристика"])
-        for i in laptops:
-            writer.writerow(["Название",i["title"],'\n',"Цена",(i["block price"]),'\n',"Просмотры" ,i["views" ],'\n',"Картины",'\n',i["image"],'\n',"Описание",'\n',i["description"],])
 
-    print("уусе!!!")
-
-
+res = []
 def execute():
-    html_content = get_response_data(HEADERS, URL)
-    if html_content.status_code == 200:
-        laptops = content(html_content.text)
-        save_csv(laptops)
-
+    for i in range(1, 15):
+        html_content = requests.get(URL + f'/?page={i}', HEADERS)
+        if html_content.status_code == 200:
+            laptops = content(html_content.text)
+            res.append(laptops)
 
 execute()
+
+def save(page):
+    with open(CSV_FILE, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Цена в долларах', 'Цена в соaмах', 'Название', 'картинка', 'описание', 'количество просмотров',] )
+        for a in page:
+            for i in a:
+                writer.writerow([i['dollars'], i['som'], i['name'], i['photo'], i['info'], i['views']])
+
+save(res)
